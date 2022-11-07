083c2e45841ccd4d48acf31e29bae9d4d0ddbbe4
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-07 22:29:34 +0600

    connect mongodb and create .env file for hide db user name and password

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const {MongoClient, ServerApiVersion} = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
-
+require('dotenv').config();
 //*middlware
 app.use(cors());
 app.use(express.json());
+
+// *start Mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qh4qhby.mongodb.net/?retryWrites=true&w=majority`;
+console.log('🚀🚀: uri', uri);
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+	const collection = client.db('test').collection('devices');
+	console.log('database connected');
+	client.close();
+});
 
 app.get('/', (req, res) => {
 	res.send('Mediz-Server is Running');
