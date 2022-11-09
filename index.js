d083634af324d2e832128c90a4079ea25587a9fa
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-09 14:12:12 +0600

    set date utc to to local Date

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 require('dotenv').config();
-
-var isodate = new Date().toISOString();
-//*middlware
 app.use(cors());
 app.use(express.json());
 
@@ -46,8 +43,10 @@ async function run() {
 	});
 	//*send review server to database
 	app.post('/review', async (req, res) => {
+		var isodate = new Date();
+		var localDateTime = isodate.toLocaleDateString() + ' ' + isodate.toLocaleTimeString();
 		const review = req.body;
-		const fullReview = {...review, date: isodate};
+		const fullReview = {...review, date: localDateTime};
 		const result = await reviewCollection.insertOne(fullReview);
 		res.send(result);
 	});
